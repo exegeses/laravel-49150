326ba366d2c2febfefc220ac38d6c8400b502c05
Marcos <exegeses@gmail.com>
2021-07-30 22:02:59 -0300

    vista de panel de administración de Destinos

--- agencia/resources/views/adminRegiones.blade.php
+++ agencia/resources/views/adminRegiones.blade.php
@@ -28,12 +28,12 @@
                     <td>{{ $region->regID }}</td>
                     <td>{{ $region->regNombre }}</td>
                     <td>
-                        <a href="/modificarRegion/regID" class="btn btn-outline-secondary">
+                        <a href="/modificarRegion/{{ $region->regID }}" class="btn btn-outline-secondary">
                             Modificar
                         </a>
                     </td>
                     <td>
-                        <a href="/eliminarRegion/regID" class="btn btn-outline-secondary">
+                        <a href="/eliminarRegion/{{ $region->regID }}" class="btn btn-outline-secondary">
                             Eliminar
                         </a>
                     </td>

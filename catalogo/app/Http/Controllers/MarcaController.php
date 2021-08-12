081533108d039e00efa49f6861934808184165d2
Marcos <exegeses@gmail.com>
2021-08-11 22:00:27 -0300

    Panel de administración de marcas

--- catalogo/app/Http/Controllers/MarcaController.php
+++ catalogo/app/Http/Controllers/MarcaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
@@ -13,7 +14,10 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        //obtener listado de marcas
+        $marcas = Marca::all();
+        //retornar vista con datos
+        return view('adminMarcas', [ 'marcas'=>$marcas ]);
     }
 
     /**
--- catalogo/routes/web.php
+++ catalogo/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+##############################################
+####### CRUD de marcas
+use App\Http\Controllers\MarcaController;
+Route::get('/adminMarcas', [ MarcaController::class, 'index' ] );
+
+
